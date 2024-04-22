5e010aa65fe5c75819c87df0143320d81f8f0395
Morgan Ugochukwu <morganugochukwu@Morgans-MacBook-Pro.local>
2024-04-22 12:26:14 +0100

    new camden video url

--- assets/data.js
+++ assets/data.js
@@ -41,7 +41,7 @@ export const projectsData = {
     ],
     map: '<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d4461.9076131820775!2d3.529210012186419!3d6.441913287929828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sChevron%2C%20Lekki%20!5e0!3m2!1sen!2sng!4v1688132862417!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
     video_url:
-      "https://res.cloudinary.com/melvinugo/video/upload/v1688654338/kirkwood/camden-video.mp4",
+      "https://res.cloudinary.com/melvinugo/video/upload/v1713637491/CAMDEN_HEIGHTS.mp4",
   },
   "woodland-court": {
     title: "Woodland Court",
