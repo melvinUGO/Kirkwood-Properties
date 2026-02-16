50214636425984eec0891244e451086c23dc2994
Melvin Ugochukwu <107886307+melvinUGO@users.noreply.github.com>
2026-02-16 08:58:54 +0100

    Add Google Maps iframe to OfficeMap component

--- components/maps/OfficeMap.jsx
+++ components/maps/OfficeMap.jsx
@@ -12,6 +12,7 @@ const OfficeMap = () => {
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.541260808103!2d3.472893375323749!3d6.452884593538586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf446995ebcb7%3A0x381028e72c27bbea!2s16a%20Chief%20Albert%20Iyorah%20St%2C%20Eti-Osa%2C%20Lekki%20105102%2C%20Lagos!5e0!3m2!1sen!2sng!4v1771228568094!5m2!1sen!2sng" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 };
